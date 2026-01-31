35271c8058729f428164533c8c8e3d02e19c1f3e
lev4ni <levani.makharashvili@gmail.com>
2026-01-31 21:17:10 +0100

    trigger

---
+++ Assets/Scripts/PlacaPresion.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using UnityEngine;
+
+public class PlacaPresion : MonoBehaviour
+{
+
+    [Header("Quién activa el trigger")]
+    public string tagObjetoActivador;
+
+
+    [Header("Qué pasa cuando se activa")]
+    public GameObject objetoActivar;
+
+    private ActivarTexto texto;
+
+    public int elementosParaActivar = 3;
+    private int elementosActuales;
+
+    public float tiempoParaActivar = 4f;
+
+    private void Start()
+    {
+        texto = FindAnyObjectByType<ActivarTexto>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!other.gameObject.CompareTag(tagObjetoActivador)) return;
+        StartCoroutine(TiempoParaActivar(tiempoParaActivar));
+
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        elementosActuales--;
+    }
+
+    private void ActivarTrigger()
+    {
+        elementosActuales++;
+        Debug.Log("Trigger activado por " + tagObjetoActivador + " -> " + elementosActuales);
+
+        if (elementosActuales >= elementosParaActivar)
+        {
+            objetoActivar.SetActive(true);
+        }
+
+        if (texto != null)
+            texto.CambiarTexto("Objetivo conseguido: Máscara! Nos vemos pronto...");
+    }
+
+    private IEnumerator TiempoParaActivar(float time)
+    {
+        yield return new WaitForSeconds(time);
+        ActivarTrigger();
+    }
+}
