a79966fbe918dfb9db3bd6cf3838984550ad7a47
lev4ni <levani.makharashvili@gmail.com>
2026-02-01 14:54:24 +0100

    ss

--- Assets/Scripts/CepoTrigger.cs
+++ Assets/Scripts/CepoTrigger.cs
@@ -8,7 +8,13 @@ public class CepoTrigger : MonoBehaviour
     private AudioSource audioSource;
     
     public Sprite cepoCerrado;
-   
+
+    private Animator animator;
+    private void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        animator = GetComponent<Animator>();
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag(tagNpc))
@@ -19,6 +25,7 @@ public class CepoTrigger : MonoBehaviour
             atraerObjeto.transform.position = transform.position + new Vector3(0, 1, 0);
             GetComponent<SpriteRenderer>().sprite = cepoCerrado;
             if(audioSource != null) audioSource.Play();
+            if (animator != null) animator.SetBool("Atrapado", true);
         }
     }
 }
