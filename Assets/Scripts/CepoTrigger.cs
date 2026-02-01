fb5ae897b836a3315a1b7308259a916a9b9d9c82
lev4ni <levani.makharashvili@gmail.com>
2026-02-01 11:52:29 +0100

    hola
    adios!

--- Assets/Scripts/CepoTrigger.cs
+++ Assets/Scripts/CepoTrigger.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 
 public class CepoTrigger : MonoBehaviour
 {
-    public string nameNpc;
+    public string tagNpc;
     private AtraerObjeto atraerObjeto;
+    public AudioSource audioJabali;
+    
+    public Sprite cepoCerrado;
+   
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.name == nameNpc)
+        if (other.gameObject.CompareTag(tagNpc))
         {
             atraerObjeto = other.GetComponent<AtraerObjeto>();
             atraerObjeto.velocidad = 0;
             atraerObjeto.puedeMoverse = false;
-          
+            atraerObjeto.transform.position = transform.position + new Vector3(0, 1, 0);
+            GetComponent<SpriteRenderer>().sprite = cepoCerrado;
+            audioJabali.Play();
         }
     }
 }
--- Assets/Scripts/DeactiveThisObject.cs
+++ Assets/Scripts/DeactiveThisObject.cs
@@ -5,11 +5,18 @@ public class DeactiveThisObject : MonoBehaviour
 {
     public float tiempoParaDestruir = 3f;
 
+
     private void Start()
     {
         StartCoroutine(Desactivacion(tiempoParaDestruir));
     }
-
+    private void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.Space)) 
+        {
+            gameObject.SetActive(false);
+        }
+    }
     IEnumerator Desactivacion(float tiempoParaDestruir)
     {
         yield return new WaitForSeconds(tiempoParaDestruir);
